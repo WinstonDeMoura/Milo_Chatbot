1cba61bae2e32454377c7025a481c7c396d94eef
Winston <winston.de.moura@gmail.com>
2025-08-05 21:28:58 -0300

    Finishing webhook

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, Request
 
+from evolution_api import send_whatsapp_message
+
 
 app = FastAPI()
 
 @app.post('/webhook')
 async def webhook(resquest: Request):
   data = await resquest.json()
-  print(data)
+  chat_id = data.get('data').get('key').get('remoteJid')
+  message = data.get('data').get('message').get('conversation')
+  
+  if chat_id and message and not '@g.us' in chat_id:
+    send_whatsapp_message(
+      number=chat_id,
+      text='Opa! Recebi!'
+    )
+  
   return {'status':'ok'}
