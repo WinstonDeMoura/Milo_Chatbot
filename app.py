5dd4731883cd51f7d29af7ec0dadd7ba4ec1f6b5
Winston <winston.de.moura@gmail.com>
2025-08-06 19:46:04 -0300

    End of coding

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request
 
+from chains import get_conversational_rag_chain
 from evolution_api import send_whatsapp_message
 
 
 app = FastAPI()
+
+conversational_rag_chain = get_conversational_rag_chain()
 
 @app.post('/webhook')
 async def webhook(resquest: Request):
@@ -12,9 +15,14 @@ async def webhook(resquest: Request):
   message = data.get('data').get('message').get('conversation')
   
   if chat_id and message and not '@g.us' in chat_id:
+    ai_response = conversational_rag_chain.invoke(
+      input={'input':message},
+      config={'configurable': {'session_id': chat_id}},
+    )['answer']
+    
     send_whatsapp_message(
       number=chat_id,
-      text='Opa! Recebi!'
+      text=ai_response,
     )
   
   return {'status':'ok'}
